b2ec98acf08a633e9b402269d59e892174276cb2
higorseixas <higor.c.seixas@gmail.com>
2023-02-07 16:00:39 -0300

    feat

---
+++ src/shared/http/routes/index.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+import productsRouter from 'src/modules/products/routes/products.routes';
+
+const routes = Router();
+
+routes.use('/products', productsRouter);
+
+export default routes;
