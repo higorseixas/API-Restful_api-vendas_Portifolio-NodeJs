9af12d95d00f9c62468b04d3afdff0d9000815e5
higorseixas <higor.c.seixas@gmail.com>
2023-02-17 21:31:32 -0300

    feat: Sessão de Autenticação com Token JWT

--- src/modules/users/services/CreatSessionsService.ts
+++ src/modules/users/services/CreatSessionsService.ts
@@ -1,19 +1,22 @@
 import AppError from '@shared/errors/AppError';
+import { compare } from 'bcryptjs';
 import { getCustomRepository } from 'typeorm';
 import User from '../typeorm/entities/User';
 import UserRepository from '../typeorm/repositories/UsersRepository';
+import { sign } from 'jsonwebtoken';
 
 interface IRequest {
   email: string;
   password: string;
 }
 
-// interface IResponse {
-//   user: User;
-// }
+interface IResponse {
+  user: User;
+  token: string;
+}
 
 class CreateSessionsService {
-  public async execute({ name, email, password }: IRequest): Promise<User> {
+  public async execute({ email, password }: IRequest): Promise<IResponse> {
     const usersRepository = getCustomRepository(UserRepository);
     const user = await usersRepository.findByEmail(email);
 
@@ -21,15 +24,18 @@ class CreateSessionsService {
       throw new AppError('Incorrect email/password combination.', 401);
     }
 
-    const passwordConfirmed = await new compare(password, user.password);
+    const passwordConfirmed = await compare(password, user.password);
 
     if (!passwordConfirmed) {
       throw new AppError('Incorrect email/password combination.', 401);
     }
 
-    await usersRepository.save(user);
+    const token = sign({}, 'b1db2d72d11624a9f4b0abb548b7bdcb', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
 
-    return user;
+    return { user, token };
   }
 }
 
