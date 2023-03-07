14c6a83d544de483899d7679708ec9cb0de46bd8
higorseixas <higor.c.seixas@gmail.com>
2023-03-07 19:42:14 -0300

    feat: Ethereal Fake Email Service

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -2,6 +2,7 @@ import AppError from '@shared/errors/AppError';
 import { getCustomRepository } from 'typeorm';
 import UserRepository from '../typeorm/repositories/UsersRepository';
 import UserTokensRepository from '../typeorm/repositories/UserTokensRepository';
+import EtherealMail from '@config/mail/EtherealMail';
 
 interface IRequest {
   email: string;
@@ -17,10 +18,12 @@ class SendForgotPasswordEmailService {
     if (!user) {
       throw new AppError('User does not exist.');
     }
-
     const token = await usersTokensRepository.generate(user.id);
 
-    
+    await EtherealMail.sendMail({
+      to: email,
+      body: `Solicitação de redefinição de senha recebida ${token?.token}`,
+    });
   }
 }
 
