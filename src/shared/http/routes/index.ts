5aa55c5148449468d7c2165f58f2035be81ade06
higorseixas <higor.c.seixas@gmail.com>
2023-03-13 11:10:30 -0300

    feat: Rotas de Clientes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,3 +1,4 @@
+import customersRouter from '@modules/customers/routes/customers.routes';
 import passwordRouter from '@modules/users/routes/password.routes';
 import sessionsRouter from '@modules/users/routes/sessions.routes';
 import usersRouter from '@modules/users/routes/users.routes';
@@ -11,5 +12,6 @@ routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/password', passwordRouter);
 routes.use('/profile', productsRouter);
+routes.use('/customers', customersRouter);
 
 export default routes;
