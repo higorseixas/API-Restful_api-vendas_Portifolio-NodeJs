6434f8b3e079b3d5f3e1f5e0403fb01ed9c66848
higorseixas <higor.c.seixas@gmail.com>
2023-03-27 20:37:21 -0300

    Criando o Provider para Upload do Avatar no Disco Local

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -3,13 +3,15 @@ import path from "path";
 import crypto from 'crypto';
 
 const uploadFolder = path.resolve(__dirname, '..', '..', 'uploads');
+const tmpFolder = path.resolve(__dirname, '..', '..', 'temp');
 
 export default {
-  diretory: uploadFolder,
+  directory: uploadFolder,
+  tmpFolder,
   storege: multer.diskStorage({
     destination: uploadFolder,
     filename(request, file, callback) {
-      const fileHash = crypto.randomBytes(10).toString('hax');
+      const fileHash = crypto.randomBytes(10).toString('hex');
       const filename = `${fileHash}-${file.originalname}`;
       callback(null, filename);
     },
