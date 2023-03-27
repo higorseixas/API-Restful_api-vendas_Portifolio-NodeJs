ccd174cf99a5d2e67a4a9f4850224fc81f11a6c2
higorseixas <higor.c.seixas@gmail.com>
2023-03-27 10:54:51 -0300

    Adicionando paginate ao projeto

--- src/modules/customers/services/ListCustomerService.ts
+++ src/modules/customers/services/ListCustomerService.ts
@@ -1,14 +1,26 @@
-// import AppError from '@shared/errors/AppError';
 import { getCustomRepository } from 'typeorm';
+import { pagination } from 'typeorm-pagination';
+import { paginate } from 'typeorm-pagination/dist/helpers/pagination';
 import Customer from '../typeorm/entities/Customer';
 import CustomersRepository from '../typeorm/repositories/CustomersRepository';
 
+interface IPaginateCustomer {
+  from: number;
+  to: number;
+  per_page: number;
+  total:number;
+  current_page: number;
+  prev_pag: number| null;
+  next_page: number | null;
+  data: Customer[];
+}
+
 export default class ListCustomerService {
-  public async execute(): Promise<Customer[]> {
+  public async execute(): Promise<IPaginateCustomer> {
     const customersRepository = getCustomRepository(CustomersRepository);
 
-    const customers = customersRepository.find();
+    const customers = await customersRepository.createQueryBuilder().paginate();
 
-    return customers;
+    return customers as unknown as IPaginateCustomer;
   }
 }
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,7 +1,10 @@
 import 'reflect-metadata';
+import 'dotenv/config';
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
+import { errors } from 'celebrate';
+import { pagination } from 'typeorm-pagination';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
 import '@shared/typeorm';
@@ -12,8 +15,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(pagination);
+
 app.use('/files', express.static(uploadConfig.diretory));
 app.use(routes);
+
+app.use(errors());
 
 app.use(
   (error: Error, request: Request, response: Response, next: NextFunction) => {
