3c6d357d0c0d0444d2e963f603e974b919214faa
higorseixas <higor.c.seixas@gmail.com>
2023-03-28 15:34:32 -0300

    Camada de Domínio - Model Customer

--- src/modules/customers/infra/typeorm/entities/Customer.ts
+++ src/modules/customers/infra/typeorm/entities/Customer.ts
@@ -5,9 +5,10 @@ import {
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
+import { ICustomer } from '@modules/customers/domain/models/ICustomer';
 
 @Entity('customer')
-class Customer {
+class Customer implements ICustomer {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
